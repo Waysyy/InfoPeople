54fefb0b8076aed1a54d5c5d10beba528f1bec48
Waysy <annatkach225@gmail.com>
2021-09-12 13:04:21 +0700

    Initial commit

---
+++ InfoPeople/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+
+namespace InfoPeople
+{
+    [Serializable]
+    public class Info
+    {
+        public List<People> PeopleList { get; set; } = new List<People>();
+
+    }
+
+    public class People
+    {
+        public string surname { get; set;}
+
+        public uint age { get; set; }
+
+        public string city { get; set; }
+        public People()
+        {
+
+        }
+        public People(string Surname, uint Age, string City)
+        {
+            this.surname = Surname;
+            this.age = Age;
+            this.city = City;
+        }
+
+    }
+
+    public static class RecordsXML
+    {
+        public static Info Get()
+        {
+            var xml = new XmlSerializer(typeof(Info));
+            using (FileStream fs = new FileStream("People.xml", FileMode.OpenOrCreate))
+            {
+                return (Info)xml.Deserialize(fs);
+            }
+        }
+
+        /*public static List<People> GetMiddleAge()
+        {
+            var records = RecordsXML.Get().PeopleList;
+            People maleRecord = new People();
+            maleRecord.Height = 0;
+            Record femaleRecord = new Record();
+            femaleRecord.Height = 0;
+
+            
+
+            var result = new List<Record> { pe, femaleRecord };
+            return result;
+        }*/
+
+
+        public static void Generate(uint num)
+        {
+            var xml = new XmlSerializer(typeof(Info));
+            var surnames = new string[] { "Иванов", "Трифонов", "Смирнов", "Кузнецов", "Соколов", "Козлов", "Сидоров" };
+            var cities = new string[] { "Кемерово", "Новокузнецк", "Москва", "Омск", "Томск","Новосибирск","Нижний Новгород","СПБ","Калининград"  };
+            
+
+            var rand = new Random();
+            string surname, city;
+            uint age;
+
+            var result = new Info();
+
+            for (int i = 0; i < num; ++i)
+            {
+                
+                    surname = surnames[rand.Next(0, 7)];
+                    city = cities[rand.Next(0, 9)];
+
+                age = Convert.ToUInt16(rand.Next(1, 99));
+                var rec = new People(surname,age,city);
+                result.PeopleList.Add(rec);
+            }
+
+            using (FileStream fs = new FileStream("People.xml", FileMode.Create))
+            {
+                xml.Serialize(fs, result);
+            }
+        }
+    }
+
+}
---
+++ InfoPeople/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace InfoPeople
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //RecordsXML.Generate(100);
+            label1.Text = Convert.ToString(Form1.GetAvg()) + "лет";
+
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RecordsXML.Generate(100);
+
+            List<People> oblst = new List<People>();
+            var list = new BindingList<People>();
+             dataGridView1.DataSource = list;
+            
+            dataGridView1.AutoGenerateColumns = true;
+
+            // Set up the data source.
+           // bindingSource1.DataSource = GetData("Select * From Products");
+
+
+        }
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           /* comboBox1.Items.Add("Кемерово");
+            comboBox1.Items.Add("Новосибирск");
+            comboBox1.Items.Add("Москва");
+            comboBox1.Items.Add("Новокузнецк");
+            comboBox1.Items.Add("Омск");
+            comboBox1.Items.Add("Томск");
+            comboBox1.Items.Add("Нижний Новгород");
+            comboBox1.Items.Add("СПБ");
+            comboBox1.Items.Add("Калининград");
+            */
+        }
+
+        public static long[] GetAvg()
+        {
+            var records = RecordsXML.Get().PeopleList;
+            uint peopleSum = 0;
+            int peopleCount = 0;
+
+            Form1 f = new Form1();
+            
+            Object selectedItem = f.comboBox1.SelectedItem;
+            MessageBox.Show("Selected Item Text: " + selectedItem.ToString());
+
+            foreach (var rec in records)
+            {
+                
+                if (rec.city == f.comboBox1.GetItemText(f.comboBox1.SelectedItem)) // не видит значения
+                {
+                    peopleSum += rec.age;
+                    peopleCount++;
+                }
+            }
+
+            var peopleAvg = peopleSum / peopleCount;
+
+
+            var result = new long[] { peopleAvg };
+            return result;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
